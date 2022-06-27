fa2618d5bdc3533c510101e563008f44f880e9b3
toshiki16 <toshiki6226@icloud.com>
2022-06-27 11:45:12 +0900

    3回目のコミット

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React from "react";
-import Header from "./Front_view/header";
+// import Header from "./Front_view/header";
 import Navibar from "./Front_view/navibar";
 import First from "./Front_view/first_view";
 
 import Footer from "./Front_view/footer";
 
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
+// import {
+//   BrowserRouter,
+//   Routes,
+//   Route
+// } from "react-router-dom";
 
 
 const App = () => {
   return (
     <>
       <Navibar />
-      {/* <Header /> */}
       <First />
       <Footer />
       {/* <BrowserRouter>
